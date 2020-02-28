df262845904f4643b1f0c9eafc1cfa1d422dac28
Dima Usatyuk <Shonen1988@gmail.com>
2020-02-28 12:24:16 +0200

    add onCompletionListener

--- app/src/main/java/com/gmail/trymediaplayer/MainActivity.java
+++ app/src/main/java/com/gmail/trymediaplayer/MainActivity.java
@@ -2,13 +2,45 @@ package com.gmail.trymediaplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mp = MediaPlayer.create(this, R.raw.test);
+
+        Button playButton = findViewById(R.id.play_btn);
+        Button pauseButton = findViewById(R.id.pause_btn);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp.start();
+            }
+        });
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp.pause();
+            }
+        });
+
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                Log.i("OnCompletionListener", "Play song completed");
+                Toast.makeText(getApplicationContext(),"I'm done",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
